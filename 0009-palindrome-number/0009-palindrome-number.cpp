292479944cf62fb4eedbd4f54bf3240b7bd26655
sandhya11-gth <sandhyabarolia11@gmail.com>
2026-08-04 16:05:23 +0530

    Time: 1 ms (57.79%), Space: 8.5 MB (92.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x<0){
+            return false;
+        }
+        int original = x;
+           long long rev= 0;
+       while(x>0){
+     
+       int digit= x %10;
+   
+        rev= rev*10 + digit;
+       x= x/10;
+       }
+      /* if (rev== original)
+        return true;
+       else 
+       return false;
+ */
+ return rev== original;
+    }
+};
