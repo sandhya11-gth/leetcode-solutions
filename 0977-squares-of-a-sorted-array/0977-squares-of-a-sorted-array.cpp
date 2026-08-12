4be6539072a86776ab8c69be1e7e1d3f96231f7f
sandhya11-gth <sandhyabarolia11@gmail.com>
2026-08-12 23:21:02 +0530

    Time: 0 ms (100%), Space: 31.9 MB (7.53%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,14 +1,73 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-       
 
+        vector<int> neg;
+        vector<int> pos;
 
- vector<int> arr;
-             for(int i=0; i<nums.size(); i++){
-    arr.push_back(nums[i]*nums[i]);
-             }
-     sort(arr.begin(), arr.end());
-return arr;
+        // Step 1: Separate negative and positive numbers
+        for(int i = 0; i < nums.size(); i++) {
+
+            if(nums[i] < 0) {
+                neg.push_back(nums[i]);
+            }
+            else {
+                pos.push_back(nums[i]);
+            }
+        }
+
+        // Step 2: Square negative numbers
+        for(int i = 0; i < neg.size(); i++) {
+            neg[i] = neg[i] * neg[i];
+        }
+
+        // Step 3: Square positive numbers
+        for(int i = 0; i < pos.size(); i++) {
+            pos[i] = pos[i] * pos[i];
+        }
+
+        // Step 4: Reverse negative squares
+        reverse(neg.begin(), neg.end());
+
+        // Now both arrays are sorted
+        // neg = [1, 4, 9]
+        // pos = [4, 9, 16]
+
+        vector<int> res(neg.size() + pos.size());
+
+        int i = 0;  // points to neg
+        int j = 0;  // points to pos
+        int id = 0; // points to res
+
+        // Step 5: Merge both arrays
+        while(i < neg.size() && j < pos.size()) {
+
+            if(neg[i] <= pos[j]) {
+                res[id] = neg[i];
+                i++;
+            }
+            else {
+                res[id] = pos[j];
+                j++;
+            }
+
+            id++;
+        }
+
+        // Remaining negative squares
+        while(i < neg.size()) {
+            res[id] = neg[i];
+            i++;
+            id++;
+        }
+
+        // Remaining positive squares
+        while(j < pos.size()) {
+            res[id] = pos[j];
+            j++;
+            id++;
+        }
+
+        return res;
     }
 };
