76c9082add109da5456f839e506894d64d49834c
SANDHYA BAROLIA <sandhyabarolia11@gmail.com>
2026-08-26 23:48:36 +0530

    Time: 2 ms (37.04%), Space: 8.3 MB (45.45%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+
+        int ans = 0;
+
+        for(int i = 0; i < 32; i++) {
+
+             int bit = n % 2;
+
+            ans = ans * 2 + bit;
+
+            n = n / 2;
+        }
+
+        return ans;
+    }
+};
