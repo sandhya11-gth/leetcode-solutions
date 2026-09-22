9af7e2520e866e5de716185ca6d766443a4b5544
SANDHYA BAROLIA <sandhyabarolia11@gmail.com>
2026-09-22 11:49:46 +0530

    Time: 0 ms (100%), Space: 13.6 MB (25.77%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int ans = 0;
+
+        for(int i = 0; i < 32; i++) {
+
+            int count = 0;
+
+            for(int x : nums) {
+
+                if(x & (1 << i)) {
+                    count++;
+                }
+            }
+
+            if(count % 3 != 0) {
+                ans = ans | (1 << i);
+            }
+        }
+
+        return ans;
+    }
+};
